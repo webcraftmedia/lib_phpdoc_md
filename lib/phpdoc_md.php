43916041af1fba6205e3b0ca4126a00c60e35de3
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2017-07-25 20:32:18 +0200

    return log as array

--- lib/phpdoc_md.php
+++ lib/phpdoc_md.php
@@ -1,34 +1,29 @@
 <?php
 class phpdoc_md {
     public static function run($inpath,$outpath){
+        $result = array();
         if (!file_exists($outpath->SERVERPATH())) {
             mkdir($outpath->SERVERPATH(), 0777, true);}
-        $named = array(
-            'lt' => '%c',
-            'index' => 'ApiIndex.md',
-        );
 
         $parser = new \PHPDocMD\Parser($inpath->SERVERPATH());
 
-        new INFO("Parsing structure.xml");
+        $result[] = 'Parsing structure.xml';
 
         $classDefinitions = $parser->run();
-
-        $templateDir = dirname(__FILE__) . '/phpdoc_md/templates/';
 
         $generator = new \PHPDocMD\Generator(
             $classDefinitions,
             $outpath->SERVERPATH(),
-            $templateDir,
-            $named['lt'],
-            $named['index']
+            dirname(__FILE__) . '/phpdoc_md/templates/',
+            '%c',
+            'ApiIndex.md'
         );
 
-        new INFO("Generating pages");
+        $result[] = 'Generating pages';
 
         $generator->run();
 
-        new INFO("Complete");
-        return \SYSTEM\LOG\JsonResult::ok();
+        $result[] = 'Complete';
+        return $result;
     }
 }
